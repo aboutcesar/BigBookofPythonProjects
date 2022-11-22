c0d7f2b3904224013225a5262dca0f70846db033
Cesar Gutierrez <cgutierrez169@gmail.com>
2022-11-22 11:43:36 -0600

    iterable list for digits

--- Bagels/bagels.py
+++ Bagels/bagels.py
@@ -35,8 +35,13 @@ def main():
             
     
 def createNumber():
-    newNumber = random.randint(100,999)
-    
+    newNumber = ''
+    a = ['0','1','2','3','4','5','6','7','8','9']
+    a.shuffle(a)
+
+    for i in range(digits):
+        newNumber.append(a[i])
+
     return newNumber 
 
 
